f9273a9c0f9abac57c2704ce7b1525d3f25d1468
emahaniuk <70599783+emahaniuk@users.noreply.github.com>
2023-03-15 21:16:45 +0800

    Add files via upload

---
+++ rand_number_rand.py
@@ -0,0 +1,9 @@
+# Import numpy as np
+import numpy as np
+
+# Set the seed (Use seed() to set the seed; as an argument, pass 123.)
+sd = np.random.seed(123)
+
+# Generate and print random float (Generate your first random float with rand() and print it out.)
+rndm_flt = np.random.rand()
+print(rndm_flt)
